889531e60404a0d586d2b5dcb22477050156330a
Quinten Leung <quinten.leung@gmail.com>
2026-03-12 04:04:19 -0700

    Format
    Signed-off-by: Quinten Leung <quinten.leung@gmail.com>

--- src/utils/RenderUtils.h
+++ src/utils/RenderUtils.h
@@ -11,8 +11,8 @@ public:
     //scaling from center point
     static SDL_FRect getScaledDest(const SDL_FRect& dest, float scale) {
         return SDL_FRect{
-            dest.x + dest.w *(1-scale)/2,
-            dest.y + dest.h *(1-scale)/2,
+            dest.x + dest.w * (1-scale) / 2,
+            dest.y + dest.h * (1-scale) / 2,
             dest.w* scale,
             dest.h* scale};
     }
